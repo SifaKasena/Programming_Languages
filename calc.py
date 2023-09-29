d25658329f67509ab618b862f4d129b4e8ecfd2f
SifaKasena <109460248+SifaKasena@users.noreply.github.com>
2023-09-29 12:58:03 +0300

    Simple sum calculator Assignment

---
+++ calc.py
@@ -0,0 +1,8 @@
+# COLLINS KASENA SIFA SCT211-0066/2022
+
+name = input("Kindly enter your name: ")
+print (f"Hello {name}, this is a simple sum calculator.\nFollow the instructions to perform your operation.")
+num1 = int(input("Kindly enter the first number: "))
+num2 = int(input("Kindly enter the second number: "))
+sum = num1 + num2
+print(f"The sum of {num1} and {num2} is: {sum}")
